66f455c23f4aeedee9ad85ac75a9809351b4a47d
aline <aline.barreto98@hotmail.com>
2023-11-22 20:49:17 -0300

    "commit com arquivo de desafio Dart finalizado"

--- desafio.dart
+++ desafio.dart
@@ -1,3 +1,20 @@
-class Logica {
-  obterNum(int num) {}
+import 'dart:io';
+
+int obterNum(int num) {
+  int soma = 0;
+  for (int numeroDigitado = 1; numeroDigitado < num; numeroDigitado++) {
+    if (numeroDigitado % 3 == 0 || numeroDigitado % 5 == 0) {
+      soma += numeroDigitado;
+    }
+  }
+  return soma;
+}
+
+void main() {
+  print("Insira um número inteiro positivo: ");
+  var input = int.parse(stdin.readLineSync()!);
+
+  var resultado = obterNum(input);
+
+  print("o resultado do processo é $resultado ");
 }
