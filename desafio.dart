5410688b8df1ebd33bb7750de051664ba4c6f20b
aline <aline.barreto98@hotmail.com>
2023-11-22 23:03:45 -0300

    "commit final."

--- desafio.dart
+++ desafio.dart
@@ -3,11 +3,10 @@ import 'dart:io';
 class MeuDesafio {
   int obterSomatorio({required int valor}) {
     int somatorio = 0;
-    //Na linha 5 abaixo, você precisa que os valores repassados sejam inferiores ao valor digitado
+
     for (int i = valor - 1; i >= 3; i--) {
-      // O menor número possível é três pois não da pra dividir por algo menor que isso
       if (i % 3 == 0 || i % 5 == 0) {
-        somatorio += i; // mesma coisa de fazer somatorio = somatorio+i;
+        somatorio += i;
         print("$somatorio  --- $i"); //printando resultado
       }
     }
