4cfe384899b10946956b7fa5e45c8608b49ebc5f
aline <aline.barreto98@hotmail.com>
2023-11-22 22:09:34 -0300

    "commit final"

--- desafio.dart
+++ desafio.dart
@@ -1,20 +1,34 @@
 import 'dart:io';
 
+//definindo a função obterNum que recebe numero como parâmetro
 int obterNum(int num) {
   int soma = 0;
+  //variável soma sendo inicalizada com 0
+  //abaixo, sendo iniciado um loop, que vai de 1 até numeroDigitado
   for (int numeroDigitado = 1; numeroDigitado < num; numeroDigitado++) {
+    //o numero digitado por padrão deve ser divisível po 3 ou 5
+    //caso contrário não "passará" para variável soma e não será exibida
+
     if (numeroDigitado % 3 == 0 || numeroDigitado % 5 == 0) {
       soma += numeroDigitado;
+      //no final a soma que inicia no zero, é adicionada ao numeroDigitado
+      //e retorna no final
     }
   }
   return soma;
 }
 
+//iniciando main:
 void main() {
   print("Insira um número inteiro positivo: ");
-  var input = int.parse(stdin.readLineSync()!);
+  // input é para ler a entrada do usuário
 
+  var input = int.parse(stdin.readLineSync()!);
+//o valor inserido pelo usuário é lido usando 'stdin.readLineSync() !=para nao ser nula'
+//e o int.parse é a forma para converter o inteiro para String
   var resultado = obterNum(input);
+
+//
 
   print("o resultado do processo é $resultado ");
 }
